c5073b5c11b3b2326d2ff3b8598a9740873ca46f
DjellzaRrustemi <djellzarrustemii@gmail.com>
2025-04-02 10:22:00 +0200

    chore: initial Xcode project setup

---
+++ QuestionApp/ViewControllers/ContentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContentViewController.swift
+//  QuestionApp
+//
+//  Created by Djellza Rrustemi  on 2.4.25.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
